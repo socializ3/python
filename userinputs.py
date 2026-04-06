483b9b237c1c3a084118d331005a142a995fd3ba
socialize <105523140+socializ3@users.noreply.github.com>
2026-04-06 16:25:47 -0700

    Add files via upload

---
+++ userinputs.py
@@ -0,0 +1,14 @@
+name = input("enter your name: ")
+age = int(input("enter your age: "))
+age = age + 1
+
+#we need to store the input inside a variable
+#adding the "name = ____" allows the code to see the input as the variable "name"
+#as you can see i added the age input, that will follow after the name question
+#i also added an int in place, allowing the variable to be changed with another line of code (3)
+#you can do the same with a float
+
+print(f"hello {name}")
+print(f"your age is {age}")
+
+
