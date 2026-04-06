bb6862ecdfad52bd49301a3e15146b2d280b566f
socialize <105523140+socializ3@users.noreply.github.com>
2026-04-06 16:27:14 -0700

    userinputs

---
+++ userinpractice.py
@@ -0,0 +1,25 @@
+#create mad libs game
+#by writing the story out then coding the userinput you can make a funny mad lib
+
+adjective1 = input("what is your adjective? ")
+noun = input("what is your noun? ")
+adjective2 = input("what is your adjective? ")
+verb = input("what is your verb? ")
+adjective3 = input("what is your adjective? ")
+
+print(f"today i went to a {adjective1} pizza parlor")
+print(f"at the front register, i saw a {noun}")
+print(f"the {noun} was {adjective2} and {verb}ing")
+print(f"i was {adjective3}")
+
+#here we have a small mad lib game, by the user inputing the words it comes out with something like this
+
+#what is your adjective? crazy
+#what is your noun? dog
+#what is your adjective? insane
+#what is your verb? madden
+#what is your adjective? hard
+#today i went to a crazy pizza parlor
+#at the front register, i saw a dog
+#the dog was insane and maddening
+#i was hard
